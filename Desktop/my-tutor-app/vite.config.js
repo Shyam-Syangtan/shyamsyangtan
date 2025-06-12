ef4a739ac15505ebc6619af341d87f72868dc76a
Shyam-Syangtan <shyamtaman97@gmail.com>
2025-06-12 13:11:36 +0530

    Update base path for new repository

--- Desktop/my-tutor-app/vite.config.js
+++ Desktop/my-tutor-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/indian-language-tutors/',
+  base: '/my-tutor-app/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
